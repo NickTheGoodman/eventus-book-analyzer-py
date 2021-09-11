88dbe117c476cd4627fad4ea2c3ce2aad5a2c618
NickTheGoodman <nick.thegoodman@gmail.com>
2021-09-11 16:25:35 -0500

    Add output logic, further cleanup, more test data

--- book_analyzer.py
+++ book_analyzer.py
@@ -1,47 +1,54 @@
 import re
 import sys
-from typing import TextIO
+from typing import TextIO, Tuple
 
-from src.bid_book import BidBook, Bid
+from src.bid_book import BidBook, Bid, UNCALCULATED_VALUE
 from src.market_log_commands import AddOrderCommand, ReduceOrderCommand, MessageTypeEnum, MarketLogCommand
 
 
 class BookAnalyzer:
     def __init__(self, target_size: int):
         self._target_size: int = target_size
-        self._book: BidBook = BidBook()
+        self._book: BidBook = BidBook(target_size)
 
     def analyze_market_log(self, log: TextIO):
-        count = 1
-
         for line in log:
-            if count == 10:
-                exit()
+            prev_bid_vars = self._book.get_state_of_bid_book()
 
             market_command = self._parse_message(line.rstrip())
 
+            if market_command is None:
+                continue
+
+            print(f" timestamp: {market_command.timestamp}")
+
             self._process_market_command(market_command)
-            print(f"book: \n{self._book}")
-            print()
 
-            count += 1
+            cur_bid_vars = self._book.get_state_of_bid_book()
 
-    def _parse_message(self, message: str) -> MarketLogCommand:
+            self._output_selling_income_if_needed(
+                market_command.timestamp,
+                self._target_size,
+                prev_bid_vars,
+                cur_bid_vars)
+
+    def _parse_message(self, msg: str) -> MarketLogCommand:
         add_order_message_pattern = r'^\d+ A \w+ (B|S) \d+\.\d{2} \d+$'
         reduce_order_message_pattern = r'^\d+ R \w+ \d+$'
 
-        add_order_match = re.compile(add_order_message_pattern).match(message)
-        reduce_order_match = re.compile(reduce_order_message_pattern).match(message)
+        add_order_match = re.compile(add_order_message_pattern).match(msg)
+        reduce_order_match = re.compile(reduce_order_message_pattern).match(msg)
 
         if add_order_match:
-            return self._parse_add_order_command(message)
+            return self._parse_add_order_command(msg)
         elif reduce_order_match:
-            return self._parse_reduce_order_command(message)
+            return self._parse_reduce_order_command(msg)
         else:
-            raise ValueError(f"Invalid market log message: {message}")
-            # TODO: Do I need to return also?
+            print_to_stderr(f"Invalid market log message: {msg}")
+            return None
 
     def _parse_add_order_command(self, message: str) -> AddOrderCommand:
+        # TODO: need to further validate that all input args are accounted for
         timestamp, message_type, order_id, side, price_str, size_str = message.split(' ')
         price_in_cents = self._to_cents(price_str)
         size = int(size_str)
@@ -49,6 +56,7 @@ class BookAnalyzer:
 
     @staticmethod
     def _parse_reduce_order_command(message: str) -> ReduceOrderCommand:
+        # TODO: need to further validate that all input args are accounted for
         timestamp, message_type, order_id, reduction_size_str = message.split(' ')
         reduction_size = int(reduction_size_str)
         return ReduceOrderCommand(timestamp, order_id, reduction_size)
@@ -59,25 +67,68 @@ class BookAnalyzer:
         return int(dollars_str)*100 + int(cents_str)
 
     def _process_market_command(self, cmd: MarketLogCommand):
+        print(f" cmd: {cmd}")
+
         if cmd.get_message_type() == MessageTypeEnum.ADD_LIMIT_ORDER:
             self._process_add_order_command(cmd)
         elif cmd.get_message_type() == MessageTypeEnum.REDUCE_LIMIT_ORDER:
             self._process_reduce_order_command(cmd)
 
     def _process_add_order_command(self, cmd: AddOrderCommand):
-        print(f"{cmd}")
-        bid = self._command_to_bid(cmd)
-        print(f"{bid}")
-        self._book.add_bid(bid)
+        self._book.add_bid(cmd.order_id, cmd.price, cmd.size)
 
     def _process_reduce_order_command(self, cmd: ReduceOrderCommand):
-        print(f"{cmd}")
-        order_id, size_reduction = cmd.order_id, cmd.size_reduction
-        self._book.reduce_bid(order_id, size_reduction)
+        self._book.reduce_bid(cmd.order_id, cmd.size_reduction)
 
     @staticmethod
     def _command_to_bid(command: AddOrderCommand) -> Bid:
         return Bid(command.order_id, command.price, command.size)
+
+    def _output_selling_income_if_needed(self, timestamp: int,
+                                         target_size: int,
+                                         prev_state_of_bid_book: Tuple[int, int, int],
+                                         cur_state_of_bid_book: Tuple[int, int, int]):
+        prev_total_bid_size, \
+            prev_price_of_cheapest_bid_to_buy, \
+            prev_selling_income = \
+            prev_state_of_bid_book
+
+        cur_total_bid_size, \
+            cur_price_of_cheapest_bid_to_buy, \
+            cur_selling_income = \
+            cur_state_of_bid_book
+
+        selling_income_has_changed = \
+            cur_selling_income != prev_selling_income
+
+        print(f" book:\t\t{self._book}")
+        print(f" bids_to_buy:\t{self._book.bids_to_buy}")
+        print(f" prev_bid_vars:\t{prev_state_of_bid_book}")
+        print(f" cur_bid_vars:\t{cur_state_of_bid_book}")
+
+        if selling_income_has_changed:
+            selling_income_to_display = self._selling_income_to_display(cur_selling_income)
+            print_to_stdout(f"{timestamp} S {selling_income_to_display}")
+
+        print()
+
+    @staticmethod
+    def _to_dollars_and_cents(price_in_cents: int) -> str:
+        dollars = str(int(price_in_cents / 100))
+        cents = str(price_in_cents % 100).zfill(2)
+        return f"{dollars}.{cents}"
+
+    def _selling_income_to_display(self, selling_income_in_cents):
+        return "NA" if selling_income_in_cents == UNCALCULATED_VALUE else \
+            self._to_dollars_and_cents(selling_income_in_cents)
+
+
+def print_to_stdout(*msg):
+    print(*msg, file=sys.stdout)
+
+
+def print_to_stderr(*msg):
+    print(*msg, file=sys.stderr)
 
 
 if __name__ == "__main__":
--- src/bid_book.py
+++ src/bid_book.py
@@ -1,8 +1,9 @@
+from collections import namedtuple
 from dataclasses import dataclass
 
-from typing import Dict, List
+from typing import Dict, List, Tuple
 
-from src.market_log_commands import SideOfBookEnum
+from src.market_log_commands import SideOfBookEnum, MessageTypeEnum
 
 
 @dataclass
@@ -10,12 +11,10 @@ class Bid:
     order_id: str
     price: int
     size: int
+    side: SideOfBookEnum = SideOfBookEnum.BID
 
-    def reduce_size(self, reduction_in_size: int):
-        self.size -= reduction_in_size
-
-    def side(self):
-        return SideOfBookEnum.BID.value
+    def reduce_size(self, size_reduction: int):
+        self.size -= size_reduction
 
 
 # All bids of the same price
@@ -60,21 +59,61 @@ class BidGroup:
         self._bid_ids.remove(order_id)
 
     def __str__(self) -> str:
-        return f"{self._price} {self._total_size} {self._bid_ids}"
+        return f"BidGroup(price={self._price}, total_size={self._total_size}, bid_ids={self._bid_ids})"
+
+
+PriceSizeTuple = namedtuple('PriceSizeTuple', [
+    'price',
+    'size'
+])
+
+StateOfBidBook = namedtuple('StateOfBidBook', [
+    'total_bid_size',
+    'price_of_cheapest_bid_to_buy',
+    'selling_income'
+])
+
+UNCALCULATED_VALUE: int = -1
 
 
 class BidBook:
-    def __init__(self):
+    def __init__(self, target_size: int):
+        self._target_size = target_size
+
         self._bids: Dict[str, Bid] = {}
         self._bid_groups: Dict[int, BidGroup] = {}
         self._sorted_prices: List[int] = []
 
-    def add_bid(self, bid: Bid):
+        self._total_bid_size = 0
+        self._bids_to_buy: List[PriceSizeTuple] = []
+        self._price_of_cheapest_bid_to_buy = UNCALCULATED_VALUE
+        self._selling_income: int = UNCALCULATED_VALUE
+
+    @property
+    def total_bid_size(self) -> int:
+        return self._total_bid_size
+
+    @property
+    def bids_to_buy(self) -> List[PriceSizeTuple]:
+        return self._bids_to_buy
+
+    @property
+    def selling_income(self) -> int:
+        return self._selling_income
+
+    def add_bid(self, order_id: str, price: int, size: int):
+        bid = Bid(order_id, price, size)
         self._add_bid_by_id(bid)
+
         if self._bid_group_already_exists(bid.price):
             self._add_bid_to_bid_group(bid)
         else:
             self._add_new_bid_group(bid)
+
+        self._update_state_of_bid_book(
+            MessageTypeEnum.ADD_LIMIT_ORDER,
+            size
+        )
 
     def _bid_group_already_exists(self, price: int) -> bool:
         return price in self._bid_groups.keys()
@@ -105,6 +144,11 @@ class BidBook:
         self._remove_bid_if_needed(bid, bid_group)
         self._remove_bid_group_if_needed(bid_group)
 
+        self._update_state_of_bid_book(
+            MessageTypeEnum.REDUCE_LIMIT_ORDER,
+            actual_size_reduction
+        )
+
     @staticmethod
     def _get_adjusted_size_reduction(bid: Bid, size_reduction: int) -> int:
         return size_reduction if bid.size - size_reduction >= 0 else bid.size
@@ -119,17 +163,69 @@ class BidBook:
 
     def _remove_bid_group_if_needed(self, bid_group: BidGroup):
         price = bid_group.price
-        should_remove_bid_price = bid_group.total_size == 0
+        should_remove_bid_group = bid_group.total_size == 0
 
-        if should_remove_bid_price:
+        if should_remove_bid_group:
             del self._bid_groups[price]
             self._sorted_prices.remove(price)
 
+    def _update_state_of_bid_book(self, msg_type: MessageTypeEnum, size_difference: int):
+        if msg_type == MessageTypeEnum.ADD_LIMIT_ORDER:
+            self._total_bid_size += size_difference
+        elif msg_type == MessageTypeEnum.REDUCE_LIMIT_ORDER:
+            self._total_bid_size -= size_difference
+
+        self._calculate_bids_to_buy()
+
+        self._calculate_selling_income()
+
+    def _calculate_bids_to_buy(self):
+        bids_to_buy: List[PriceSizeTuple] = []
+        price_of_cheapest_bid_to_buy = UNCALCULATED_VALUE
+
+        if self._should_calculate():
+            num_shares_left_to_buy = self._target_size
+
+            for price in self._sorted_prices:
+                bid_group = self._bid_groups[price]
+
+                if num_shares_left_to_buy >= bid_group.total_size:
+                    num_shares_to_buy = bid_group.total_size
+                    bids_to_buy.append(PriceSizeTuple(price, num_shares_to_buy))
+                    num_shares_left_to_buy -= bid_group.total_size
+                else:
+                    bids_to_buy.append((price, num_shares_left_to_buy))
+                    price_of_cheapest_bid_to_buy = price
+                    break
+
+        self._bids_to_buy = bids_to_buy
+        self._price_of_cheapest_bid_to_buy = price_of_cheapest_bid_to_buy
+
+    def _calculate_selling_income(self):
+        selling_income = UNCALCULATED_VALUE
+
+        if self._should_calculate():
+            selling_income = 0
+            for (price, size) in self._bids_to_buy:
+                selling_income += price * size
+
+        self._selling_income = selling_income
+
+    def _should_calculate(self):
+        return self._total_bid_size >= self._target_size
+
+    def get_state_of_bid_book(self) -> Tuple[int, int, int]:
+        return StateOfBidBook(
+            self._total_bid_size,
+            self._price_of_cheapest_bid_to_buy,
+            self._selling_income
+        )
+
     def __str__(self):
-        bid_prices_str = ""
+        bid_group_str = ""
         for i, price in enumerate(self._sorted_prices):
-            bid_price = self._bid_groups[price]
-            bid_prices_str += f"{bid_price}"
+            bid_group = self._bid_groups[price]
+            bid_group_str += f"{bid_group}"
             if i < len(self._sorted_prices) - 1:
-                bid_prices_str += "\n"
-        return bid_prices_str
+                bid_group_str += ", "
+        return bid_group_str
